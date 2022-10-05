5a7da1ea51e9bc5fb0377b850120efbd6a1ca45a
Paul <paulswann44@gmail.com>
2022-10-05 15:32:34 -0500

    OCT 5th 2022

---
+++ js/external.js
@@ -0,0 +1,46 @@
+let greeting = "Hello from external Javascript";
+console.log(greeting);
+
+let myAlert = alert('Welcome to my website');
+console.log(myAlert);
+
+let myPrompt = prompt('What is your favorite color');
+console.log(myPrompt);
+
+let favColor = alert("Great, blue is my favorite color too!");
+console.log(favColor);
+
+//Exercise 3, Part I
+let price = 3;
+let movieOne = prompt("How many days did you rent Little Mermaid?");
+let priceOfMermaid = movieOne * price;
+console.log(alert("The price of the Little Mermaid is " + priceOfMermaid));
+
+let movieTwo = prompt("How many days did you rent Brother Bear?");
+let priceOfBear = movieTwo * price;
+console.log(alert("The price of the Brother Bear is " + priceOfBear));
+
+let movieThree =prompt('How many days did you rent Hercules?');
+let priceOfHercules = price * movieThree;
+console.log(alert("The price of the Hercules is " + priceOfHercules));
+
+
+
+//Exercise 3, Part II
+let googlePay = prompt("What is the pay from Google?");
+let googleHours = prompt("How many hours? did you work for Google?");
+let results = googlePay * googleHours;
+console.log(alert("This will be your pay " + results ));
+
+let applePay = prompt("What is the pay from Amazon?");
+let appleHours = prompt("How many hours did you work for Amazon?");
+let sumOfPay = applePay * appleHours;
+console.log(alert("This will be your pay " + sumOfPay));
+
+let amazonPay = prompt("What is the pay from Facebook?");
+let  amazonHours= prompt("How many hours did you work for Facebook?");
+let totalPay = amazonPay * amazonHours;
+console.log(alert("This will be your pay " + totalPay));
+
+
+//Exercise 3, Part III
---
+++ js/intro-js.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let greeting = "hello from an external JS file!";
+console.log(greeting);
+//without "use strict", will appear fine, WITH "use strict, appears red
+//let myAge = 25;
+
+//alert("You've been hacked");
+
+//stores the result of the prompt
+//let userName = prompt('What is your name');
+//log the result of the prompt
+//console.log(userName);
+
+//let isFull = onfirm("Is the class full?");
+//console.log(isFull);
+
+alert("Hello," + userName + " welcome to my website!");
