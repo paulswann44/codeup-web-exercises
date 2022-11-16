a5406b9cecbf8aee9f8198edd49dbd155c719cdc
paulswann44 <99698080+paulswann44@users.noreply.github.com>
2022-11-15 21:48:18 -0600

    jQuery

---
+++ jQuary-lec.js
@@ -0,0 +1,16 @@
+"use strict";
+(function(){
+
+    //object.method
+    //  BOM object.method
+    // window.onload = function() {
+    //     alert( 'The page has finished loading!' );
+    // }
+    //jQuery alias is $
+    // $(function() {
+    //     alert("The DOM has finished loading")
+    // });
+    jQuery(function() {
+        alert("The DOM has finished loading")
+    });
+})();
---
+++ js/jquery_exercise.js
@@ -0,0 +1,15 @@
+"use strict";
+(function() {
+
+// $(function() {
+//     alert("The DOM has finished loading")
+// });
+
+
+    $(function () {
+        alert("my first jQuery function.... #hello_world!")
+    });
+
+
+
+}())
