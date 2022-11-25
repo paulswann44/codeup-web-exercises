56c0ca55cff5ecb6a5b74387b704f9ebd41c98cb
paulswann44 <99698080+paulswann44@users.noreply.github.com>
2022-11-24 18:36:25 -0600

    fuck javascript

---
+++ js/weather_map.js
@@ -0,0 +1,71 @@
+/*
+* NOTES:
+*(1)Draggable Cursor
+*https://docs.mapbox.com/mapbox-gl-js/example/drag-a-marker/
+*
+* (2) Web Dev Simplified
+* https://www.youtube.com/watch?v=OySigNMXOZU
+*
+* */
+
+
+
+// Token access
+
+var accessToken = MAPBOX_TOKEN;
+mapboxgl.accessToken = MAPBOX_TOKEN;
+
+
+
+
+//creates map (given)
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [-96.7970, 32.7767],   //The center need to change by city search
+    zoom: 10,
+
+});
+
+
+// Create "+" and "-" navigation tool on the right corner (Note 2)
+const nav = new mapboxgl.NavigationControl()
+map.addControl(nav)
+
+
+//Creates a marker and establishes that it is draggable (Note 1)
+let marker = new mapboxgl.Marker({
+    draggable: true
+})
+
+    .setLngLat([-96.7970, 32.7767])
+    .addTo(map);
+
+marker.on('dragend', onDragEnd);
+
+
+//Draggable Function (Note 1)
+function onDragEnd() {
+    const lngLat = marker.getLngLat();
+    coordinates.style.display = 'block';
+    coordinates.innerHTML = `Longitude: ${lngLat.lng}<br />Latitude: ${lngLat.lat}`;
+}
+
+
+
+
+
+
+// When clicked the "city-search" (class name) button geolocates the city
+$('#btn').click(function (){  //click submit button to activate function
+    geocode($('#search').val(), MAPBOX_TOKEN).then(function(coordinates) { //this converts the address to coordinates by calling the function
+        map.setCenter(coordinates)  //It should reset the center to the coordinates from the data
+        setWeatherData(coordinates[0],coordinates[1]) //I want the lat & long
+        map.setLngLat(coordinates) //A marker should be placed onto the cordinates
+        console.log(coordinates)
+    })
+
+
+
+
+})
