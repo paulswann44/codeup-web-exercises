0b65dae89eb3743cc469242b3b7b849a7d4beee8
paulswann44 <99698080+paulswann44@users.noreply.github.com>
2022-11-27 15:29:54 -0600

    updates

--- js/weather_map.js
+++ js/weather_map.js
@@ -31,20 +31,20 @@ let map = new mapboxgl.Map({
 // Add zoom and rotation controls to the map (Note #1)
 map.addControl(new mapboxgl.NavigationControl());
 
-const marker = new mapboxgl.Marker({
-    draggable: true
-})
-    .setLngLat([-96.7970, 32.7767])
-    .addTo(map);
+// const marker = new mapboxgl.Marker({
+//     draggable: true
+// })
+//     .setLngLat([-96.7970, 32.7767])
+//     .addTo(map);
 
-function onDragEnd() {
-    const lngLat = marker.getLngLat();//Produces this object when dragged- {lng: -96.74206835937004, lat: 32.71606049688401}
-    // console.log('lat:' + lngLat.lat, '/typeof: ' + typeof lngLat.lat,'/lng:'+ lngLat.lng, '/typeof: ' + typeof lngLat.lng);
-    coordinates.style.display = 'block';
-    coordinates.innerHTML = `Longitude: ${lngLat.lng}</br>Latitude: ${lngLat.lat}`;
-}
+// function onDragEnd() {
+//     const lngLat = marker.getLngLat();//Produces this object when dragged- {lng: -96.74206835937004, lat: 32.71606049688401}
+// console.log('lat:' + lngLat.lat, '/typeof: ' + typeof lngLat.lat,'/lng:'+ lngLat.lng, '/typeof: ' + typeof lngLat.lng);
+// coordinates.style.display = 'block';
+// coordinates.innerHTML = `Longitude: ${lngLat.lng}</br>Latitude: ${lngLat.lat}`;
+// }
 
-marker.on('dragend', weatherData());
+// marker.on('dragend', getWeatherData());
 
 
 $('#btn').click(
@@ -53,63 +53,90 @@ $('#btn').click(
             function getLatLong(coordinates) { //this converts the address to lng & lat by calling the function
                 let lng = coordinates[0]; //longitude of submitted address location
                 let lat = coordinates[1]; //latitude of submitted address location
-                map.setCenter(coordinates)  //It should reset the center to the coordinates from the data
-                weatherData(lat, lng)
+                map.setCenter(coordinates)
+                map.setZoom(10)
+
+                let marker = new mapboxgl.Marker({draggable: true})
+                    .setLngLat([coordinates[0], coordinates[1]])
+                    .addTo(map)
                 marker.setLngLat(coordinates)// -_-  it is not Map....It is marker
+
+
+                //sets weather coodinates from input
+                getWeatherData(coordinates)
+
+
+
                 reverseGeocode({lat, lng}, MAPBOX_TOKEN).then(function address(results) {
-                    // $('#city-name').html(results); //see note (2) below, but intend to delete
-                    let popup = new mapboxgl.Popup({className: 'edit-popup'}) //This generates a popup with a custom class
-                        .setLngLat(coordinates)//Sets the pop at the searched destination
-                        .setHTML(`Your location is:<br> ${results} <br> Your coordinates are: <br> ${coordinates}`)//Displays the physical address
-                        .addTo(map);//adds it to the map
+                    $('#city-name').html(results); //see note (2) below, but intend to delete
+
                 });
             });
     });
 
-/*
-* NOTE #2
-* I will delete later because I can possibly use the JSON file, so it just shows the city
-*
-* */
-//+++++++++++++++++++++++++++++++++++++++WEATHER GET REQUEST++++++++++++++++++++++++++++++++++++
-
-function weatherData () {
+function getWeatherData(coordinates) {
     $.get("http://api.openweathermap.org/data/2.5/forecast", { //This is the only endpoint that will not cause me an error
         APPID: OPEN_WEATHER_APPID,
-        lat: marker.getLngLat().lat,  //it should produce a number with lat coordinate wherever the marker moves
-        lon: marker.getLngLat().lng, // it should produce a number with long coordinate - wherever the marker moves
+        lat: coordinates[1],  //it should produce a number with lat coordinate wherever the marker moves
+        lon: coordinates[0], // it should produce a number with long coordinate - wherever the marker moves
         units: "imperial",
         exclude: 'minutely,hourly,current,alerts'
     }).done(function (data) {
-        $('.date').each(function(){
-            $(this).html('date path goes here'); //Endpoint won't work
-        });
-        $('.temprature').each(function(){  //each temprature should have this
-            $(this).html('temprature path goes here'); //Endpoint won't work
-        });
-        $('.humidity').each(function(){
-            $(this).html('humidity path goes here'); //Endpoint won't work
-        });
-        $('.pressure').each(function(){
-            $(this).html('pressure path goes here'); //Endpoint won't work
-        });
-        $('.wind').each(function(){
-            $(this).html('wind path goes here'); //Endpoint won't work
-        });
-        $('.wind').each(function(){
-            $(this).html('wind path goes here'); //Endpoint won't work
-        });
-        $('#city-name').html('City name path goes here');
 
-        console.log('The entire response:', data);
-        console.log('Diving in - here is current information: ', data.current);
-        console.log('A step further - information for tomorrow: ', data.daily[1]);//is it daily needed?
+        let forecasts = data.list
+        console.log("Data List: ", data.list)
+        let forecastHTML = append(forecasts)
+        $('#weather').html(forecastHTML)
+        console.log("forecast: ", forecasts)
+
     }).fail(function (jqXhr, status, error) {
         console.log(jqXhr);
         console.log(status);
         console.log(error);
     });
 }
+
+
+let append = function (data) {
+    let html = ``
+    for (let i = 0; i < data.length; i += 8) {
+        console.log("Data: ", data[i])
+        const {
+            dt_txt,
+            main: {humidity, temp, temp_max, temp_min},
+            weather: [{description, icon}],
+            wind: {speed}
+        } = data[i]
+        html += `
+            <div class="card  col-lg">
+               <h6 class="card-header text-center " >${dt_txt.substring(5, 7)}.${dt_txt.substring(8, 10)}.${dt_txt.substring(0, 4)}
+               </h6>
+               <img src='http://openweathermap.org/img/w/${icon}.png' class="img-thumbnail mx-auto d-block border-0" style='width: 100px; height: 100px;' alt="...">
+              <div class="card-body pt-0">
+              <h4 class="card-title text-center">${temp.toFixed(1)}ºF</h4>
+                <div class="d-flex justify-content-around" >
+                <p class="card-text mb-0 text-warning pe-1">
+                H:${temp_max.toFixed(1)}ºF
+                </p>
+
+                <p class="card-text mb-0 text-warning" >
+                L:${temp_min.toFixed(1)}ºF
+                </p>
+                </div>
+
+              </div>
+              <ul class="list-group list-group-flush text-center">
+                <li class="list-group-item  humidity">Humidity: ${humidity}%</li>
+                <li class="list-group-item  wind">Wind: ${speed} mph</li>
+              </ul>
+              <div class="card-body p-0 ">
+              <p class="list-group-item bg-secondary text-light text-center p-0 m-0">${description.toUpperCase()}</p>
+              </div>
+            </div>`
+    }
+    return html
+}
+
 
 
 
