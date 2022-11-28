850e8f5821cae8fac9530fdcaf88c17b9880726b
paulswann44 <99698080+paulswann44@users.noreply.github.com>
2022-11-28 10:43:48 -0600

    ...

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,3 +1,4 @@
+(function () {
 "use strict";
 
 
@@ -10,62 +11,72 @@ mapboxgl.accessToken = MAPBOX_TOKEN;
 let map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v9',
-    center: [-96.7970, 32.7767],   //The center need to change by city search
-    zoom: 10
+    center: [-96.7970, 32.7767],
+    zoom: 3
 
 });
 
 map.addControl(new mapboxgl.NavigationControl());
 
-// let marker = new mapboxgl.Marker({
-//     draggable: true
-// })
-//     .setLngLat([-96.7970, 32.7767])
-//     .addTo(map);
+const marker = new mapboxgl.Marker({
+    draggable: true
+})
+    .setLngLat([-96.7970, 32.7767])
+    .addTo(map);
 
-// marker.on('dragend', function onDragEnd() {
-//     const lngLat = marker.getLngLat();//Produces this object when dragged- {lng: -96.74206835937004, lat: 32.71606049688401}
-//     console.log('lat:' + lngLat.lat, '/typeof: ' + typeof lngLat.lat, '/lng:' + lngLat.lng, '/typeof: ' + typeof lngLat.lng);
-//     coordinates.style.display = 'block';
-//     coordinates.innerHTML = `Longitude: ${lngLat.lng}</br>Latitude: ${lngLat.lat}`;
-// });
+function onDragEnd() {
+    const lngLat = marker.getLngLat();
+    coordinates.style.display = 'block';
+    coordinates.innerHTML = `Longitude: ${lngLat.lng}<br />Latitude: ${lngLat.lat}`;
+    console.log('Drag function','lat:' + lngLat.lat, '/typeof: ' + typeof lngLat.lat, '/lng:' + lngLat.lng, '/typeof: ' + typeof lngLat.lng);
+
+}
+
+marker.on('dragend', onDragEnd);
+
+
 
 
 $('#btn').click(
-    function renderPhysicalAddress() {  //When I click the button
+    function renderPhysicalAddress() {
         geocode($('#search').val(), MAPBOX_TOKEN, map).then(
-            function getLatLong(coordinates) { //this converts the address to lng & lat by calling the function
-                let lng = coordinates[0]; //longitude of submitted address location
-                let lat = coordinates[1]; //latitude of submitted address location
+            function getLatLong(coordinates) {
+                let lng = coordinates[0];
+                let lat = coordinates[1];
                 map.setCenter(coordinates)
                 map.setZoom(10)
 
                 let marker = new mapboxgl.Marker({draggable: true})
                     .setLngLat([coordinates[0], coordinates[1]])
                     .addTo(map)
-                marker.setLngLat(coordinates)// -_-  it is not Map....It is marker
+                marker.setLngLat(coordinates);
                 getWeatherData(coordinates)
-                reverseGeocode({lat, lng}, MAPBOX_TOKEN).then(function address(results) {
-                    $('#city-name').html(results); //see note (2) below, but intend to delete
 
-                });
+
             });
     });
 
 function getWeatherData(coordinates) {
-    $.get("http://api.openweathermap.org/data/2.5/forecast", { //This is the only endpoint that will not cause me an error
+    $.get("http://api.openweathermap.org/data/2.5/forecast", {
         APPID: OPEN_WEATHER_APPID,
-        lat: coordinates[1],  //it should produce a number with lat coordinate wherever the marker moves
-        lon: coordinates[0], // it should produce a number with long coordinate - wherever the marker moves
+        lat: coordinates[1],
+        lon: coordinates[0],
         units: "imperial",
         exclude: 'minutely,hourly,current,alerts'
     }).done(function (data) {
-
+console.log(data);
         let forecasts = data.list
         console.log("Data List: ", data.list)
-        let forecastHTML = append(forecasts)
-        $('#weather').html(forecastHTML)
+        let appendedForecast = append(forecasts);
+        $('#weather').html(appendedForecast);
         console.log("forecast: ", forecasts)
+
+        //City Name
+        $('#city-name').html(data.city.name);
+        console.log("Data City: ", data.city.name)
+
+
+
 
     }).fail(function (jqXhr, status, error) {
         console.log(jqXhr);
@@ -87,25 +98,23 @@ let append = function (data) {
         } = data[i]
         html += `
             <div class="card  col-xxl">
-               <h6 class="card-header text-center " >${dt_txt.substring(5, 7)}.${dt_txt.substring(8, 10)}.${dt_txt.substring(0, 4)}
+               <h6 class="card-header date text-center " >${dt_txt.substring(5, 7)}/${dt_txt.substring(8, 10)}/${dt_txt.substring(0, 4)}
                </h6>
-               <img src='http://openweathermap.org/img/w/${icon}.png' class="img-thumbnail mx-auto d-block border-0" style='width: 100px; height: 100px;' alt="...">
+               <img src='http://openweathermap.org/img/w/${icon}.png' class="img-thumbnail mx-auto d-block border-0 img" ;' alt="image">
               <div class="card-body pt-0">
               <h4 class="card-title text-center">${temp.toFixed(1)}ºF</h4>
                 <div class="d-flex justify-content-around" >
                 <p class="card-text mb-0 text-warning pe-1">
-                H:${temp_max.toFixed(1)}ºF
+                H: ${temp_max.toFixed(1)}ºF
                 </p>
-
                 <p class="card-text mb-0 text-warning" >
-                L:${temp_min.toFixed(1)}ºF
+                L: ${temp_min.toFixed(1)}ºF
                 </p>
                 </div>
-
               </div>
               <ul class="list-group list-group-flush text-center">
-                <li class="list-group-item  humidity">Humidity: ${humidity}%</li>
-                <li class="list-group-item  wind">Wind: ${speed} mph</li>
+                <li class="list-group-item humidity">Humidity: ${humidity}%</li>
+                <li class="list-group-item wind">Wind: ${speed} mph</li>
               </ul>
               <div class="card-body p-0 ">
               <p class="list-group-item bg-secondary text-light text-center p-0 m-0">${description.toUpperCase()}</p>
@@ -117,4 +126,5 @@ let append = function (data) {
 
 
 
-
+})();
+// data.
