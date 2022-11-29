33367d49259e7af6b5abe0deb93f2ece26e45eb7
paulswann44 <99698080+paulswann44@users.noreply.github.com>
2022-11-28 21:59:06 -0600

    ...

--- js/weather_map.js
+++ js/weather_map.js
@@ -14,7 +14,8 @@
 
     });
 
-    map.addControl(new mapboxgl.NavigationControl());
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav, 'top-left');
 
 
 
@@ -24,29 +25,28 @@
                 function getLatLong(coordinates) {
                     //Resets the map
                     map.setCenter(coordinates)
-                    map.setZoom(10)
+                    map.setZoom(3)
+
+
 
                     //Drops a marker at the new location
-                    let marker = new mapboxgl.Marker({color: 'crimson'})
-                    marker.setDraggable(coordinates)
-                        .setLngLat([coordinates[0], coordinates[1]])
-                        .addTo(map)
-                    marker.setLngLat(coordinates)
-                    marker.on('dragend', function onDragEnd() {
-                        const lngLat = marker.getLngLat();
-                        // coordinates.style.display = 'block';
-                        // coordinates.innerHTML = `Longitude: ${lngLat.lng}</br>Latitude: ${lngLat.lat}`;
-                        // getWeatherData(lngLat)
-                       let coordinates = lngLat.toArray()
-                        console.log(lngLat.toArray())
-                        // console.log(lngLat)
+                        let marker = new mapboxgl.Marker({color: 'crimson'})
+
+                        marker.setDraggable(coordinates)
+                            .setLngLat([coordinates[0], coordinates[1]])
+                            .addTo(map)
+
+
+                        marker.on('dragend', function onDragEnd() {
+                            const lngLat = marker.getLngLat();
+                            let coordinates = lngLat.toArray()
+                            console.log(lngLat.toArray())
+                            getWeatherData(coordinates)
+                            // console.log('Drag function:', 'lat:' + lngLat.lat, '/typeof: ' + typeof lngLat.lat, '/lng:' + lngLat.lng, '/typeof: ' + typeof lngLat.lng);
+                        });
+
+                        //Relay coordinates to weatherData
                         getWeatherData(coordinates)
-
-                        // console.log('Drag function', 'lat:' + lngLat.lat, '/typeof: ' + typeof lngLat.lat, '/lng:' + lngLat.lng, '/typeof: ' + typeof lngLat.lng);
-                    });
-
-                    //Relay coordinates to weatherData
-                    getWeatherData(coordinates)
                 });
         });
 
@@ -54,8 +54,6 @@
     function getWeatherData(coordinates) {
         $.get("http://api.openweathermap.org/data/2.5/forecast", {
             APPID: OPEN_WEATHER_APPID,
-            // lat: coordinates[1],
-            // lon: coordinates[0],
             lat: coordinates[1],
             lon: coordinates[0],
             units: "imperial",
@@ -65,7 +63,7 @@
 
             //appending the weather
             let forecasts = data.list
-            // console.log("Data List: ", data.list)
+            console.log("Data List: ", data.list)
             let appendedForecast = append(forecasts);
             $('#weather').html(appendedForecast);
             // console.log("forecast: ", forecasts)
@@ -78,9 +76,9 @@
 
 
         }).fail(function (jqXhr, status, error) {
-            // console.log(jqXhr);
-            // console.log(status);
-            // console.log(error);
+            console.log(jqXhr);
+            console.log(status);
+            console.log(error);
         });
     }
 
@@ -97,18 +95,14 @@
             } = data[i]
             html += `
             <div class="card  col-xxl">
-               <h6 class="card-header date text-center " >${dt_txt.substring(5, 7)}/${dt_txt.substring(8, 10)}/${dt_txt.substring(0, 4)}
-               </h6>
+               <h6 class="card-header date text-center ">${dt_txt.substring(5, 7)}/${dt_txt.substring(8, 10)}/${dt_txt.substring(0, 4)}</h6>
                <img src='http://openweathermap.org/img/w/${icon}.png' class="img-thumbnail mx-auto d-block border-0 img" ;' alt="image">
+               
               <div class="card-body pt-0">
               <h4 class="card-title text-center">${temp.toFixed(1)}ºF</h4>
                 <div class="d-flex justify-content-around" >
-                <p class="card-text mb-0 text-warning pe-1">
-                H: ${temp_max.toFixed(1)}ºF
-                </p>
-                <p class="card-text mb-0 text-warning" >
-                L: ${temp_min.toFixed(1)}ºF
-                </p>
+                <p class="card-text mb-0 text-warning pe-1"> H: ${temp_max.toFixed(1)}ºF </p>
+                <p class="card-text mb-0 text-warning" > L: ${temp_min.toFixed(1)}ºF</p>
                 </div>
               </div>
               <ul class="list-group list-group-flush text-center">
@@ -125,4 +119,3 @@
 
 
 })();
-// data.
