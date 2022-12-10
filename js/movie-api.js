334ee94949ad6b66d8a177bc8dd05fe5af5bcf0b
paulswann44 <99698080+paulswann44@users.noreply.github.com>
2022-12-09 20:35:52 -0600

    ...

--- js/movie-api.js
+++ js/movie-api.js
@@ -5,6 +5,12 @@
 const movieKey = MOVIE_API
 let hideLoading = $('.lds-ring').hide();
 
+//URL for the glitch database
+const url = "https://axiomatic-private-utahceratops.glitch.me/movies";
+
+//This gets the live and accurate update of the POST request from glitch
+fetch(url).then(res => res.json()).then(data => console.log(data));
+
 
 //This is a function that stores the input value
 $('#submit-btn').click((event) => {
@@ -24,43 +30,44 @@ const loadingAnimation = () => {
 }
 
 
+//Get request to the omdb API
 const getMovies = (search) => {
     loadingAnimation()
     fetch(`https://www.omdbapi.com?apikey=${movieKey}&s=${search}`)
         .then(response => response.json())//then... return json
         .then(function (data) { //then return data
             // console.log('data', data);
-            loadingAnimation()
+            loadingAnimation();
 
 
             let movie = data.Search;
-            // console.log('data.Search: ', data.Search)
-            let newMovies = movie.map(obj => obj.Title)
 
 
-            let appendMovies = append(movie)
-            $('#append-movies').html(appendMovies)  //I want to take the results and place it in the div w/ ID of movies
+            let appendMovies = append(movie);
+            $('#append-movies').html(appendMovies) ;
 
 
         })
         .catch((error) => {
-            loadingAnimation()
+            loadingAnimation();
             console.log(error);
         })
 }
 
-
+//Appends into a card when information if fetched
 const append = function (data) {
     let html = ``
     for (let i in data) {
         // console.log("Data: ", data[i])
         const {Title, Year, Poster} = data[i]
-        html += `<div class="container parent${i++} " id="parent${i++}">
+        html += `<div class="container parent${i++} " id="parent${i++} mt-5">
             <button type="button" class="btn-close remove-card" id="delete" onclick="parentNode.remove()"></button> 
                          <img src="${Poster}">
                          <h5> ${Title}</h5>
+                         <div>
                          <p>Movie Year: ${Year}</p>
                          <p>price is $2.99</p>
+                         </div>
            <button type="button" class="btn" id="addToCart" onclick="addToCart('${Title}','${Year}','${Poster}');parentNode.remove()">Add Movie</button>  
             </div>`
 
@@ -71,11 +78,11 @@ const append = function (data) {
 
 /*
 * This creates the cart where it stores the value of the array
-*
+* Might or might not be useful
 * */
 let cart = [];
 
-function addToCart(title, year, poster) {
+const addToCart = (title, year, poster) => {
     let item = {
         title: title,
         year: year,
@@ -83,11 +90,52 @@ function addToCart(title, year, poster) {
 
     };
     cart.push(item);
-    console.log('cart', cart)
-    console.log(cart.length)
+    console.log('cart', cart);
+    console.log(cart.length);
 
 }
 
 
 
+const postMovie = movie =>{
+    console.log(movie)
+    movie.preventDefault()
+    const movieObj = movie;
+    const option =   {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(movieObj),
+    };
+    console.log('post option', option)
+    fetch(url, option)
+        .then(response => response.json())
+        .then(function (data) {
+            // alert('Post successful')
+            console.log('data', data);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
 
+}
+let post= document.getElementById('new-movie');
+post.addEventListener('submit', postMovie);
+
+
+
+// Listen for clicks on the "Checkout" button
+document.getElementById("checkout").addEventListener("click", openModal);
+// Listen for clicks on the "Close" button
+document.getElementById("close-modal").addEventListener("click", closeModal);
+
+
+/*
+* (1) POST ADDed MOVIES TO THE DATABASE/GLITCH
+* (2) CREATE A MODAL THAT POP-UP'S FOR CHECK OUT (PUT)
+*                   (A) Can Edit the order quantity (PATCH)
+*                   (B) Calculate subtotal and total
+*                   (C) Input that puts in special code to get 20% discount
+*                   (D) DELETE MOVIES FROM THE LIST
+* */
